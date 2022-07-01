f8e4bca1d1b2d1840a15334f4230d8ebdaa7a615
DooVN <108518880+DooVN@users.noreply.github.com>
2022-07-01 16:04:04 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var btnClose = document.querySelector('.bx-menu')
+var sideBar = document.querySelector('.main')
+btnClose.addEventListener('click', function(){
+    sideBar.classList.toggle('close')
+})
+
+
